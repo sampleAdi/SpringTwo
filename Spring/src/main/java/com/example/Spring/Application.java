d8c46436a230f0d31e79d0984cfeaab29fdcb842
AdityaTiwari <tiwari.adi2008@gmail.com>
2025-03-02 19:07:15 +0530

    Project uploaded till UC2

--- Spring/src/main/java/com/example/Spring/Application.java
+++ Spring/src/main/java/com/example/Spring/Application.java
@@ -1,6 +1,7 @@
 package com.example.Spring;
 
 import com.example.Spring.component.Demobean;
+import com.example.Spring.component.EmployeeBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +16,10 @@ public class Application {
 		logger.debug("Welcome to Spring Concept Demo");
 		ApplicationContext context = SpringApplication.run(Application.class, args);
 		logger.debug("Checking Context: {}", context.getBean(Demobean.class));
-		logger.debug("\nExample Using @Autowired annotation on property ===");
+		logger.debug("\n** Example Using @Autowire annotation on property **");
+		EmployeeBean employeeBean = context.getBean(EmployeeBean.class);
+		employeeBean.setEid(104);
+		employeeBean.setEname("Spring Framework Guru");
+		employeeBean.showEmployeeDetails();
 	}
 }
